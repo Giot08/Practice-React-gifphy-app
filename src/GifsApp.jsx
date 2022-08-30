2dec1b5ac72a49de980f5628a8e6ae86917236db
giot08 <xgiot08@gmail.com>
2022-08-30 14:00:45 -0400

    75

--- src/GifsApp.jsx
+++ src/GifsApp.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+
 export const GifsApp = () => {
+  const [categories, setCategories] = useState(["Power Rangers", "Samurai X"]);
+
+  const onAddCategory = () => {
+    setCategories([...categories, "Power Rangers red"]);
+
+  }
+
+
   return (
     <>
-    <h1>Gifs App</h1>
+      {/* titulo */}
+      <h1>Gifs App</h1>
+
+      {/* input */}
+      <input type="text" />
+
+
+      {/* listado Gifs */}
+      <button onClick={onAddCategory}>Agregar</button>
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
-  )
-}
+  );
+};
