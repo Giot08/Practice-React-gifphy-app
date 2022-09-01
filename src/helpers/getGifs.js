12e47dee45b39dd73a36d4012522e5e775252075
giot08 <xgiot08@gmail.com>
2022-08-31 22:57:30 -0400

    81

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,16 +1,15 @@
-export const getGifs = async () => {
+export const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=zX6tMlSr7RzCRsf1AEZpHe4m7B31Pr18&q=${category}&limit=20`;
+  const resp = await fetch(url);
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=zX6tMlSr7RzCRsf1AEZpHe4m7B31Pr18&q=${category}&limit=20`
-    const resp = await fetch(url);
+  const { data } = await resp.json();
 
-    const { data } = await resp.json();
+  const gifs = data.map((img) => ({
+    id: img.id,
+    title: img.title,
+    url: img.images.downsized_medium.url,
+  }));
 
-    const gifs = data.map(img => ({
-      id: img.id,
-      title: img.title,
-      url: img.images.downsized_medium.url,
-
-    }))
-
-    console.log(gifs)
-  }
+  console.log(gifs);
+  return gifs;
+};
